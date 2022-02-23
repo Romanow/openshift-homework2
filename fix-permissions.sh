668a8eeb2db90c1f5f1abc3f9c88c8b2c212de1c
aromanov <aromanov@inno.tech>
2022-02-23 13:15:16 +0300

    add fix persmissions script

---
+++ fix-permissions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+namespaces=$(oc get namespace | awk 'NR > 1 {print $1;}' | grep -v openshift)
+
+for namespace in $namespaces; do
+  echo "Processing $namespace"
+  cat <<EOF | oc apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-role-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: admin
+subjects:
+  - kind: ServiceAccount
+    name: default
+    namespace: $namespace
+EOF
+done
