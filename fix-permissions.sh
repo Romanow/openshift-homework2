b27b2a60fd6741593d7ff8a855c999611e74eb2a
aromanov <aromanov@inno.tech>
2022-02-23 13:43:43 +0300

    add fix persmissions script

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-namespaces=$(oc get namespace | awk 'NR > 1 {print $1;}' | grep -v openshift | grep -v kube)
+namespaces=$(oc get namespace | awk 'NR > 1 {print $1;}' | grep -v '^openshift' | grep -v '^kube')
 
 for namespace in $namespaces; do
   echo "Processing $namespace"
@@ -8,7 +8,7 @@ for namespace in $namespaces; do
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
-  name: admin-role-binding
+  name: $namespace-role-binding
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
